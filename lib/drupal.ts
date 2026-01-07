31d30c08fd66c4b909792c243b3ee27d6dc8d09c
michaelbrownjr <mikeabrownjr+github@gmail.com>
2026-01-06 18:41:57 -0700

    Disable auth to allow public anonymous access and fix 404s

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -3,9 +3,9 @@ import { DrupalClient } from "next-drupal";
 export const drupal = new DrupalClient(
   process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || "",
   {
-    auth: {
-      clientId: process.env.DRUPAL_CLIENT_ID || "valid_id", // Fallback to avoid build error if missing
-      clientSecret: process.env.DRUPAL_CLIENT_SECRET || "valid_secret",
-    },
+    // auth: {
+    //   clientId: process.env.DRUPAL_CLIENT_ID || "valid_id", // Fallback to avoid build error if missing
+    //   clientSecret: process.env.DRUPAL_CLIENT_SECRET || "valid_secret",
+    // },
   }
 );
